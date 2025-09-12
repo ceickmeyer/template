36cff66050550cef7162e39d8a6bda8d8abcd9e7
ceickmeyer <cody.eickmeyer@gmail.com>
2025-09-11 21:48:53 -0400

    refactor: remove unused TweetCard import from create page

--- src/routes/admin/create/+page.svelte
+++ src/routes/admin/create/+page.svelte
@@ -2,8 +2,7 @@
 <script lang="ts">
   import TweetCreator from '$lib/components/TweetCreator.svelte';
 	import TweetParser from '$lib/components/TweetParser.svelte';
-	import TweetCard from '$lib/components/ui/TweetCard.svelte';
-  
+	  
 </script>
 
 <svelte:head>
